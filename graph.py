e2183660c1efc4798d4a577a32c1a7bf453d9325
Alex Guo <aguo2@uiowa.edu>
2024-11-07 19:43:19 -0600

    added docs

--- graph.py
+++ graph.py
@@ -4,24 +4,23 @@ class Graph:
         self.out_neighbors = out
         self.weights = weights
 
-
+# I am making the case for 2 functions. One has weights and one does not. 
+# One is a CSV and one is not. 
 def loadNetwork_txt():
     with open('soc-Epinions1.txt', 'r') as file:
         in_neighbors = {}
         out_neighbors = {}
-        for line in file:
-            if not (line[0] == '#'):
+        for line in file: #iterate over lines of files
+            if not (line[0] == '#'): # lines starting with '#' are comments, ignore those
                 a,b = line.split()
-                a.strip()
-                b.strip()
-                in_l = in_neighbors.get(b,[])
-                in_l.append(a)
+                in_l = in_neighbors.get(b,[]) # get or defualt the empty list
+                in_l.append(a) # add a to the adj. list
                 in_neighbors[b] = in_l
-                out_l = in_neighbors.get(a,[])
+                out_l = out_neighbors.get(a,[]) #do the same for out_neighbors
                 out_l.append(b)
                 out_neighbors[a] = out_l
                 #no weights on this graph!
-    res = Graph(in_neighbors, out_neighbors)
+    res = Graph(in_neighbors, out_neighbors) #create the graph and return it
     return res
 
 
@@ -29,5 +28,5 @@ def loadNetwork_txt():
 def loadNetwork_csv():
     pass
 
-
-loadNetwork_txt()
+g = loadNetwork_txt()
+print(g.out_neighbors)
